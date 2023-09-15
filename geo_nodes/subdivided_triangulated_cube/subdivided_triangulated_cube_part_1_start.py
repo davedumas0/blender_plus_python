427a5836ab87be2ee85c139ff1ff2b801db06d62
davedumas0 <davedumas0@yahoo.com>
2023-09-15 13:40:26 -0700

    Update subdivided_triangulated_cube_part_1_start.py

--- geo_nodes/subdivided_triangulated_cube/subdivided_triangulated_cube_part_1_start.py
+++ geo_nodes/subdivided_triangulated_cube/subdivided_triangulated_cube_part_1_start.py
@@ -159,14 +159,325 @@ def scene_setup():
 
     set_scene_props(fps, frame_count)
 
+def create_node(node_tree, type_name):
+    node_obj = node_tree.nodes.new(type=type_name)   
+    return node_obj
+
+def move_node(type_name, gx=0, gy=0):
+    """
+    Move a node by the specified amount in the x and y directions.
+
+    Parameters:
+    - node: The node to be moved.
+    - dx: The amount to move the node in the x direction.
+    - dy: The amount to move the node in the y direction.
+    """
+    type_name.location.x += gx
+    type_name.location.y += gy
+
+def create_link(node_tree, from_node, output_socket_name, to_node, input_socket_name):
+    """
+    Create a link between two nodes using the specified output and input socket names.
+
+    Parameters:
+    - node_tree: The node tree in which the nodes reside.
+    - from_node: The source node from which the connection originates.
+    - output_socket_name: The name of the output socket on the source node.
+    - to_node: The destination node to which the connection leads.
+    - input_socket_name: The name of the input socket on the destination node.
+    """
+    node_tree.links.new(from_node.outputs[output_socket_name], to_node.inputs[input_socket_name])
+    
+    
+def add_geo_nodes_modifier(obj):
+    """
+    Adds a Geometry Nodes modifier to the given object and sets it as the active object.
+
+    Parameters:
+    - obj: The object to which the modifier should be added.
+    """
+    # Ensure the object is selected and active
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(True)
+
+    # Add the Geometry Nodes modifier
+    bpy.ops.node.new_geometry_nodes_modifier()
+    node_tree = obj.modifiers["GeometryNodes"].node_group
+    return node_tree
+
+def clear_nodes(node_tree):
+    """
+    Clears all nodes from the specified node tree.
+
+    Parameters:
+    - node_tree: The node tree from which nodes should be removed.
+    """
+    for node in list(node_tree.nodes):  # Use list() to avoid issues during iteration
+        node_tree.nodes.remove(node)
+        
+        
+   
+def create_mesh(mesh_type='PLANE', location=(0, 0, 0)):
+    """
+    Create a mesh of the specified type.
+
+    Parameters:
+    - mesh_type: Type of the mesh to be created ('PLANE', 'CUBE', 'SPHERE', etc.)
+    - location: Location where the mesh should be created
+    """
+    if mesh_type == 'PLANE':
+        bpy.ops.mesh.primitive_plane_add(location=location)
+    elif mesh_type == 'CUBE':
+        bpy.ops.mesh.primitive_cube_add(location=location)
+    elif mesh_type == 'SPHERE':
+        bpy.ops.mesh.primitive_uv_sphere_add(location=location)
+    # ... Add more mesh types as needed
+    return active_object()
+
+# Usage:
+# plane = create_mesh('PLANE', location=(0, 0, 0))
+# cube = create_mesh('CUBE', location=(1, 1, 1))
+# sphere = create_mesh('SPHERE', location=(2, 2, 2))
+
+def move_object(obj, translation_vector=(0, 0, 0)):
+    """
+    Translates an object by the specified vector.
+
+    Parameters:
+    - obj: The object to be moved.
+    - translation_vector: A tuple (dx, dy, dz) specifying the translation in each axis.
+    """
+    obj.location += bpy.Vector(translation_vector)
+    
+# Usage (commented out for reference):
+# move_object(my_object, (1, 0, 0))  # Move the object 1 unit in the x-axis
+
+
+def scale_object(obj, scale_factor=(1, 1, 1)):
+    """
+    Scales an object by the specified factor.
+
+    Parameters:
+    - obj: The object to be scaled.
+    - scale_factor: A tuple (sx, sy, sz) specifying the scaling in each axis.
+    """
+    obj.scale *= bpy.Vector(scale_factor)
+
+# Usage (commented out for reference):
+# scale_object(my_object, (2, 2, 2))  # Double the object's size in all axes
+ 
+
+def enter_edit_mode(obj):
+    """
+    Switches the given object to edit mode.
+
+    Parameters:
+    - obj: The object to be switched to edit mode.
+    """
+    # Ensure the object is selected
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(True)
+
+    # Switch to edit mode
+    bpy.ops.object.mode_set(mode='EDIT')
+    
+# Usage (commented out for reference):
+# enter_edit_mode(my_object)  # Switch the object to edit mode
+
+def exit_edit_mode(obj):
+    """
+    Switches the given object back to object mode.
+
+    Parameters:
+    - obj: The object to be switched back to object mode.
+    """
+    # Ensure the object is selected
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(True)
+
+    # Switch to object mode
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+# Usage (commented out for reference):
+# exit_edit_mode(my_object)   # Switch the object back to object mode
+
+
+def set_selection_mode(obj, mode='FACE'):
+    """
+    Set the selection mode in Edit Mode (VERTEX, EDGE, FACE).
+
+    Parameters:
+    - obj: The object to set the selection mode for.
+    - mode: The selection mode ('VERTEX', 'EDGE', 'FACE').
+    """
+    # Check if the object is in edit mode, if not, switch to edit mode
+    if obj.mode != 'EDIT':
+        enter_edit_mode(obj)
+    
+    # Set the selection mode
+    if mode == 'VERTEX':
+        bpy.ops.mesh.select_mode(type='VERT')
+    elif mode == 'EDGE':
+        bpy.ops.mesh.select_mode(type='EDGE')
+    elif mode == 'FACE':
+        bpy.ops.mesh.select_mode(type='FACE')
+    else:
+        print(f"Unsupported mode: {mode}. Supported modes are VERTEX, EDGE, and FACE.")
+
+# Usage (commented out for reference):
+# set_selection_mode(my_object, mode='FACE')  # Set the object's selection mode to face selection
+
+def duplicate_mesh(obj):
+    """
+    Duplicates a mesh object and returns the duplicate.
+
+    Parameters:
+    - obj: The mesh object to be duplicated.
+    """
+    # Ensure the object is selected and active
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(True)
+    
+    # Duplicate the object
+    bpy.ops.object.duplicate_move()
+    
+    # The duplicated object will be the active object after the operation
+    duplicate_obj = bpy.context.active_object
+    
+    return duplicate_obj
+
+
+def duplicate_node(node_tree, node):
+    """
+    Duplicates a node within a node tree and returns the duplicate.
+
+    Parameters:
+    - node_tree: The node tree in which the node resides.
+    - node: The node to be duplicated.
+    """
+    # Ensure the node tree is active
+    bpy.context.space_data.node_tree = node_tree
+    
+    # Make sure only the desired node is selected
+    for n in node_tree.nodes:
+        n.select = False
+    node.select = True
+    node_tree.nodes.active = node
+    
+    # Duplicate the node
+    bpy.ops.node.duplicate()
+    
+    # The duplicated node will be the active node after the operation
+    duplicate_node = node_tree.nodes.active
+    
+    return duplicate_node
+
+def get_socket_names(node):
+    """
+    Returns the names of input and output sockets (pins) of a node.
+
+    Parameters:
+    - node: The node whose socket names are to be retrieved.
+
+    Returns:
+    - A dictionary with 'inputs' and 'outputs' keys.
+    """
+    socket_info = {
+        'inputs': [socket.name for socket in node.inputs],
+        'outputs': [socket.name for socket in node.outputs]
+    }
+    
+    return socket_info
+
+def get_active_geo_nodes_viewport_area():
+    """
+    Returns the active VIEW_3D area in the Geometry Nodes tab if Blender is in Object Mode.
+    
+    Returns:
+    - The active VIEW_3D area in the Geometry Nodes tab, or None if there isn't one.
+    """
+    if bpy.context.mode != 'OBJECT':
+        print("Not in Object Mode!")
+        return None
+
+    # Check if we're in the Geometry Nodes tab
+    if bpy.context.screen.name != 'Geometry Nodes':
+        print("Not in the Geometry Nodes tab!")
+        return None
+
+    for area in bpy.context.screen.areas:
+        if area.type == 'VIEW_3D':
+            return area
+
+    return None
+
+def set_active_3d_view_to_wireframe():
+    """
+    Sets the shading mode of the active 3D Viewport in the Geometry Nodes tab/workspace to wireframe.
+    """
+    # Get the active 3D Viewport in the Geometry Nodes workspace
+    active_3d_view = get_active_geo_nodes_viewport_area()
+
+    if active_3d_view:
+        # Set shading mode to wireframe
+        active_3d_view.spaces[0].shading.type = 'WIREFRAME'
+    else:
+        print("No active 3D Viewport found in the Geometry Nodes workspace.")
+
 
 ################################################################
 # helper functions END
 ################################################################
+nodeList = {
+    "GeometryNodeInstanceOnPoints",
+    "NodeGroupInput",
+    "NodeGroupOutput",
+    "GeometryNodeDistributePointsOnFaces",
+    "GeometryNodePointScale",
+    "GeometryNodeAttributeTransfer",
+    "GeometryNodeVectorMath",
+    "GeometryNodeAttributeMath",
+    "GeometryNodeAttributeProximity",
+    "GeometryNodePointSeparate",
+    "GeometryNodePointCombine",
+    "GeometryNodeAttributeCombine",
+    "GeometryNodeAttributeFill",
+    "GeometryNodeAttributeRandomize",
+    "GeometryNodeAttributeCurve",
+    "GeometryNodeAttributeColorRamp",
+    "GeometryNodeAttributeClamp",
+    "GeometryNodeAttributeMix",
+    "GeometryNodePointTranslate",
+    "GeometryNodePointRotate",
+    "GeometryNodePointScale",
+    "GeometryNodeAttributeNormal",
+    "GeometryNodeAttributeTangent",
+    "GeometryNodeAttributeUVMap",
+    "GeometryNodeAttributePosition",
+    "GeometryNodeAttributeBounds",
+    "GeometryNodeAttributeProximity",
+    "GeometryNodePointInstance",
+    "GeometryNodeMeshPrimitive",
+    "GeometryNodeAttributeBoolean",
+    "GeometryNodeSubdivideMesh",
+    # Add more nodes as needed
+}
+
+
+################################################################
+# workspace BEGIN
+################################################################
+
+
+
 
 
 def create_centerpiece():
     pass
+################################################################
+# workspace END
+################################################################
+
 
 
 def main():
@@ -180,3 +491,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+#[ONLY MAKE CHANGES IN THE WORKING AREA]
